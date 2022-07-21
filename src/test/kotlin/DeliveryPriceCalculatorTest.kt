8d0f1d51e348b451eda8b412e9e84ce65ad6aa6f
Rogov Alexander <rogovalex@yandex-team.ru>
2022-07-21 22:38:58 +0300

    pairwise and limit value testing

--- src/test/kotlin/DeliveryPriceCalculatorTest.kt
+++ src/test/kotlin/DeliveryPriceCalculatorTest.kt
@@ -23,40 +23,23 @@ class DeliveryPriceCalculatorTest(
         @Parameterized.Parameters(name = "{0} {1} {2} {3} = {4}")
         fun params(): Iterable<Array<*>> {
             return listOf(
-                // ALL DISTANCES / Size.LARGE / Fragility.REGULAR / ServiceLoad.EXTREME
-                arrayOf(Int.MAX_VALUE, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 800),
-                arrayOf(30_001, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 800),
-                arrayOf(30_000, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 640),
-                arrayOf(10_001, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 640),
-                arrayOf(10_000, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 480),
-                arrayOf(2_001, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 480),
-                arrayOf(2_000, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 400),
-                arrayOf(1, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 400),
-
-                // Int.MAX_VALUE / Size.LARGE / Fragility.REGULAR / REMAINING SERVICE LOAD
-                arrayOf(Int.MAX_VALUE, Size.LARGE, Fragility.REGULAR, ServiceLoad.HIGH, 700),
-                arrayOf(Int.MAX_VALUE, Size.LARGE, Fragility.REGULAR, ServiceLoad.INCREASED, 600),
+                // pairwise and limit value
                 arrayOf(Int.MAX_VALUE, Size.LARGE, Fragility.REGULAR, ServiceLoad.REGULAR, 500),
-
-                // Int.MAX_VALUE / REMAINING SIZE / Fragility.REGULAR / ServiceLoad.EXTREME
-                arrayOf(Int.MAX_VALUE, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 640),
-
-                // 30_000 / Size.LARGE / REMAINING FRAGILITY / ServiceLoad.EXTREME
-                arrayOf(30_000, Size.LARGE, Fragility.FRAGILE, ServiceLoad.EXTREME, 1120),
-
-                // ALL DISTANCES / Size.SMALL / Fragility.REGULAR / ServiceLoad.EXTREME
-                arrayOf(40_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 640),
-                arrayOf(20_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 480),
-                arrayOf(5_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 400),
-                arrayOf(1_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 400),
-
-                // 40_001 / Size.SMALL / Fragility.REGULAR / REMAINING SERVICE LOAD
-                arrayOf(40_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.HIGH, 560),
-                arrayOf(40_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.INCREASED, 480),
-                arrayOf(40_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.REGULAR, 400),
-
-                // 1_000 / Size.SMALL / REMAINING FRAGILITY / ServiceLoad.EXTREME
-                arrayOf(1_000, Size.SMALL, Fragility.FRAGILE, ServiceLoad.EXTREME, 720),
+                arrayOf(1_000_000, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 640),
+                arrayOf(100_000, Size.LARGE, Fragility.REGULAR, ServiceLoad.INCREASED, 600),
+                arrayOf(30_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.HIGH, 560),
+                arrayOf(30_000, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 640),
+                arrayOf(25_000, Size.SMALL, Fragility.FRAGILE, ServiceLoad.REGULAR, 600),
+                arrayOf(15_000, Size.LARGE, Fragility.REGULAR, ServiceLoad.INCREASED, 480),
+                arrayOf(10_001, Size.SMALL, Fragility.FRAGILE, ServiceLoad.HIGH, 840),
+                arrayOf(10_000, Size.SMALL, Fragility.REGULAR, ServiceLoad.HIGH, 400),
+                arrayOf(7_000, Size.LARGE, Fragility.FRAGILE, ServiceLoad.EXTREME, 960),
+                arrayOf(4_000, Size.SMALL, Fragility.REGULAR, ServiceLoad.REGULAR, 400),
+                arrayOf(2_001, Size.LARGE, Fragility.FRAGILE, ServiceLoad.INCREASED, 720),
+                arrayOf(2_000, Size.LARGE, Fragility.REGULAR, ServiceLoad.INCREASED, 400),
+                arrayOf(1_500, Size.SMALL, Fragility.FRAGILE, ServiceLoad.HIGH, 630),
+                arrayOf(500, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 400),
+                arrayOf(1, Size.SMALL, Fragility.FRAGILE, ServiceLoad.REGULAR, 450),
             )
         }
     }
