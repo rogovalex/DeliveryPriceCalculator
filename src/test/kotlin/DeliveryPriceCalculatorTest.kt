8745a925e3b0be8502571ecee4321391f03aa6af
Rogov Alexander <rogovalex@yandex-team.ru>
2022-07-21 17:54:41 +0300

    updated tests

--- src/test/kotlin/DeliveryPriceCalculatorTest.kt
+++ src/test/kotlin/DeliveryPriceCalculatorTest.kt
@@ -1,4 +1,3 @@
-import DeliveryPriceCalculator.MIN_PRICE
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,57 +23,41 @@ class DeliveryPriceCalculatorTest(
         @Parameterized.Parameters(name = "{0} {1} {2} {3} = {4}")
         fun params(): Iterable<Array<*>> {
             return listOf(
-                // large fragile extreme
-                arrayOf(30_000, Size.LARGE, Fragility.FRAGILE, ServiceLoad.EXTREME, 1120),
-                arrayOf(10_000, Size.LARGE, Fragility.FRAGILE, ServiceLoad.EXTREME, 960),
-                arrayOf(2_000, Size.LARGE, Fragility.FRAGILE, ServiceLoad.EXTREME, 880),
-
-                // small fragile extreme
-                arrayOf(30_000, Size.SMALL, Fragility.FRAGILE, ServiceLoad.EXTREME, 1120),
-                arrayOf(10_000, Size.SMALL, Fragility.FRAGILE, ServiceLoad.EXTREME, 960),
-                arrayOf(2_000, Size.SMALL, Fragility.FRAGILE, ServiceLoad.EXTREME, 880),
-
-                // large regular extreme
+                // ALL DISTANCES / Size.LARGE / Fragility.REGULAR / ServiceLoad.EXTREME
                 arrayOf(Int.MAX_VALUE, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 800),
-                arrayOf(30_000, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 1120),
-                arrayOf(10_000, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 960),
-                arrayOf(2_000, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 880),
+                arrayOf(30_001, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 800),
+                arrayOf(30_000, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 640),
+                arrayOf(10_001, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 640),
+                arrayOf(10_000, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 480),
+                arrayOf(2_001, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 480),
+                arrayOf(2_000, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 400),
+                arrayOf(1, Size.LARGE, Fragility.REGULAR, ServiceLoad.EXTREME, 400),
 
-                // small regular extreme
-                arrayOf(Int.MAX_VALUE, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 800),
-                arrayOf(30_000, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 1120),
-                arrayOf(10_000, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 960),
-                arrayOf(2_000, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 880),
+                // Int.MAX_VALUE / Size.LARGE / Fragility.REGULAR / REMAINING SERVICE LOAD
+                arrayOf(Int.MAX_VALUE, Size.LARGE, Fragility.REGULAR, ServiceLoad.HIGH, 700),
+                arrayOf(Int.MAX_VALUE, Size.LARGE, Fragility.REGULAR, ServiceLoad.INCREASED, 600),
+                arrayOf(Int.MAX_VALUE, Size.LARGE, Fragility.REGULAR, ServiceLoad.REGULAR, 500),
 
+                // Int.MAX_VALUE / REMAINING SIZE / Fragility.REGULAR / ServiceLoad.EXTREME
+                arrayOf(Int.MAX_VALUE, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 640),
 
+                // 30_000 / Size.LARGE / REMAINING FRAGILITY / ServiceLoad.EXTREME
+                arrayOf(30_000, Size.LARGE, Fragility.FRAGILE, ServiceLoad.EXTREME, 1120),
 
-                arrayOf(1, Size.LARGE, Fragility.FRAGILE, ServiceLoad.EXTREME, 880),
+                // ALL DISTANCES / Size.SMALL / Fragility.REGULAR / ServiceLoad.EXTREME
+                arrayOf(40_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 640),
+                arrayOf(20_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 480),
+                arrayOf(5_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 400),
+                arrayOf(1_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.EXTREME, 400),
 
+                // 40_001 / Size.SMALL / Fragility.REGULAR / REMAINING SERVICE LOAD
+                arrayOf(40_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.HIGH, 560),
+                arrayOf(40_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.INCREASED, 480),
+                arrayOf(40_001, Size.SMALL, Fragility.REGULAR, ServiceLoad.REGULAR, 400),
 
-
-                arrayOf(31_002, Size.SMALL, Fragility.REGULAR, ServiceLoad.REGULAR, MIN_PRICE),
-                arrayOf(16_500, Size.SMALL, Fragility.REGULAR, ServiceLoad.REGULAR, MIN_PRICE),
-                arrayOf(9_040, Size.SMALL, Fragility.REGULAR, ServiceLoad.REGULAR, MIN_PRICE),
-                arrayOf(1_234, Size.SMALL, Fragility.REGULAR, ServiceLoad.REGULAR, MIN_PRICE),
-                arrayOf(31_002, Size.LARGE, Fragility.REGULAR, ServiceLoad.REGULAR, 500),
-                arrayOf(16_500, Size.LARGE, Fragility.REGULAR, ServiceLoad.REGULAR, MIN_PRICE),
-                arrayOf(9_040, Size.LARGE, Fragility.REGULAR, ServiceLoad.REGULAR, MIN_PRICE),
-                arrayOf(1_234, Size.LARGE, Fragility.REGULAR, ServiceLoad.REGULAR, MIN_PRICE),
+                // 1_000 / Size.SMALL / REMAINING FRAGILITY / ServiceLoad.EXTREME
+                arrayOf(1_000, Size.SMALL, Fragility.FRAGILE, ServiceLoad.EXTREME, 720),
             )
         }
-//        - более 30 км: +300 рублей к доставке;
-//        - до 30 км: +200 рублей к доставке;
-//        - до 10 км: +100 рублей к доставке;
-//        - до 2 км: +50 рублей к доставке;
-
-//        - большие габариты: +200 рублей к доставке;
-//        - маленькие габариты: +100 рублей к доставке;
-
-//        Если груз хрупкий — +300 рублей к доставке. Хрупкие грузы нельзя возить на расстояние более 30 км;
-
-//        - очень высокая загруженность — 1.6;
-//        - высокая загруженность — 1.4;
-//        - повышенная загруженность — 1.2;
-//        - во всех остальных случаях коэффициент равен 1.
     }
 }
